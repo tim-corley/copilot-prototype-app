92c520dd9ebe0337c152b42e2f06d23b6324f363
Tim Corley <tcorley215@gmail.com>
2022-04-09 15:32:52 -0400

    adding todo notes on seed.py

--- server/seed.py
+++ server/seed.py
@@ -9,6 +9,9 @@ import django
 # sys.path.append(BASE_DIR)
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
+# TODO move script to tools/ directory & figure out import / server.setting issue
+# TODO move csv file to flight/ dir and adjust open() arg
+
 django.setup()
 
 from flight.models import Airport, Plane, Duty, Leg, Receipt
@@ -16,6 +19,5 @@ from flight.models import Airport, Plane, Duty, Leg, Receipt
 with open('airportdata-raw.csv') as airportfile:
     data = csv.reader(airportfile, delimiter=',')
     for row in itertools.islice(data, 0, 9):
-        print(f"CODE: {row[0]}\nSTATE: {row[1]}\nNAME: {row[2]}")
         Airport(icao=f"{row[0]}", state=f"{row[1]}", facility_name=f"{row[2]}", lat_standard=float(row[3]), lat_radian=float(row[4]), lon_standard=float(row[5]), lon_radian=float(row[6])).save()
-        print("\n\nAirport data has been successfully seeded.")
+    print("Airport data has been successfully seeded.")
