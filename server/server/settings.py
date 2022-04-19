3bb615e31245f6996a3784ae4e76ebf80ff0712a
Tim Corley <tcorley215@gmail.com>
2022-04-19 15:11:16 -0400

    adding mail dir & dockerfile for mailhog
    updated docker-compose.dev to include mail container

--- server/server/settings.py
+++ server/server/settings.py
@@ -102,7 +102,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # EMAIL CONFIG
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "localhost"
+EMAIL_HOST = "172.21.0.4"
 EMAIL_PORT = "1025"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
